e336c4a051cbaccf6e3e2e051cab90ff01604939
Nazar Kotsyuba <kotsuyba@gmail.com>
2020-05-03 13:17:12 +0200

    fix: instanceof check issue

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 module.exports = function logFormData(formData) {
-    if (formData instanceof FormData) throw new TypeError("Isn't a FormData object!");
+    if (formData instanceof FormData === false) throw new TypeError("Isn't a FormData object!");
 
     for (var pair of formData.entries()) {
         console.log(pair[0] + ", " + pair[1]);
