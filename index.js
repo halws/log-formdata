80fd05b827e1ab32221fee8fcbcc384b6f615102
Nazar Kotsyuba <kotsuyba@gmail.com>
2020-05-03 12:48:25 +0200

    refactor function

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-function logFormData(formData) {
+module.exports = function logFormData(formData) {
+    if (formData instanceof FormData) throw new TypeError("Isn't a FormData object!");
+
     for (var pair of formData.entries()) {
         console.log(pair[0] + ", " + pair[1]);
     }
 }
-
-module.exports = logFormData
